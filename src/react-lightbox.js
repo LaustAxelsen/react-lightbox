093093b5a0842161319a06ba6b4ead8373589b12
christianalfoni <christianalfoni@gmail.com>
2015-03-24 11:43:58 +0100

    Initial commit

---
+++ src/react-lightbox.js
@@ -0,0 +1,11 @@
+var React = require('react');
+var DOM = React.DOM;
+
+var Lightbox = React.createClass({
+  render: function () {
+    return DOM.h1(null, 'hello world');
+  }
+})
+
+
+module.exports = Lightbox;
